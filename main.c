5bb5aee74e3c5531c86edfaffc7e63a02c992bd9
matejmachac <mail.machac@gmail.com>
2014-12-26 19:43:47 +0100

    test change

--- main.c
+++ main.c
@@ -1,25 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include <string.h>
 
-int main()
+ main()
 {
     char line[100];
 
     puts("Assignment 3.4");
-    puts("Name: SelectLines");
-    puts("Description: Prints all lines with odd length from the standard input. All other lines are ignored.");
-    puts("**********************");
-
-    while(fgets(line,100,stdin)!=0)
-    {
-        if((strlen(line)-1)%2!=0)
-        {
-            printf("%s",line);
-        }
-    }
-
-    return 0;
-}
-
